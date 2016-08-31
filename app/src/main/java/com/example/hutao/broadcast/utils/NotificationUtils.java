191f12e5e2d90521d81d6eeab43dbc21c86caccd
hutao <285279904@qq.com>
2016-08-31 21:01:41 +0800

    提交工具类

--- app/src/main/java/com/example/hutao/broadcast/utils/NotificationUtils.java
+++ app/src/main/java/com/example/hutao/broadcast/utils/NotificationUtils.java
@@ -3,9 +3,6 @@ package com.example.hutao.broadcast.utils;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
-import android.content.Intent;
-
-import com.example.hutao.broadcast.R;
 
 /**
  * Created by hutao on 2016-08-13.
@@ -13,19 +10,13 @@ import com.example.hutao.broadcast.R;
 
 public class NotificationUtils {
 
-    private static final int id = 517;
-    
-    public static void showNotification(Context context, String title, String text,Intent intent) {
-        NotificationManager notificationManager = (NotificationManager)
-                context.getSystemService(Context.NOTIFICATION_SERVICE);
+    private NotificationManager manager;
 
-        Notification.Builder builder = new Notification.Builder(context);
-        builder.setTicker(title)
-                .setContentTitle(title)
-                .setContentText(text)
-                .setWhen(System.currentTimeMillis())
-                .setSmallIcon(R.mipmap.ic_launcher);
+    public NotificationUtils(Context context) {
+        manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+    }
 
-        notificationManager.notify(id, builder.build());
+    public void notify(int id, Notification notification) {
+        manager.notify(id, notification);
     }
 }
